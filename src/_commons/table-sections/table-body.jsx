427e89966761d881f9084ed6860bbb62d1056be9
Cristian <cristian@MacBook-Pro-de-Cristian.local>
2020-07-18 20:52:32 -0500

    feat: select validation done

--- src/_commons/table-sections/table-body.jsx
+++ src/_commons/table-sections/table-body.jsx
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import _ from 'lodash';
+import React, { Component } from "react";
+import _ from "lodash";
 class TableBody extends Component {
-
   renderCell = (item, column) => {
     if (column.functionality) return column.functionality(item);
     return _.get(item, column.path);
-  }
+  };
 
   render() {
     const { rowsDataByPage, tableHeaderFields } = this.props;
     return (
       <tbody>
-        {rowsDataByPage.map((item, index) =>
+        {rowsDataByPage.map((item, index) => (
           <tr>
             <th scope="row">{index + 1}</th>
-            {tableHeaderFields.map(column => 
-              <td>{this.renderCell(item, column)}</td>)}
+            {tableHeaderFields.map((column) => (
+              <td>{this.renderCell(item, column)}</td>
+            ))}
           </tr>
-        )}
+        ))}
       </tbody>
     );
   }
